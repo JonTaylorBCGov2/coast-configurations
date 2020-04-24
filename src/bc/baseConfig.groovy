5e1b9e3e0f51de88bd2745285c8841504092551b
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-04-24 09:01:55 -0700

    Update baseConfig.groovy

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -3,17 +3,17 @@ package bc;
 
 class baseConfig {
   // Wait timeout in minutes
-  public static final int WAIT_TIMEOUT = 10
+  public static final int WAIT_TIMEOUT = 20
 
   // Deployment Environment TAGs
   public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'bc-test', 'bc-prod']
 
   // The name of the project namespace(s).
-  public static final String  NAME_SPACE = 'devex-von-bc-tob'
+  public static final String  NAME_SPACE = 'pssg-cscp-vsd'
 
   // Instance Suffix
-  public static final String  SUFFIX = '-indy-cat'
+  public static final String  SUFFIX = '-tools'
 
   // Apps - Listed in the order they should be tagged
-  public static final String[] APPS = ['db', 'search-engine', 'msg-queue', 'frontend', 'agent', 'backup', 'schema-spy', 'api']
+  public static final String[] APPS = ['vsd']
 }
